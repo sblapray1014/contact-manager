16b5bf4f749611959c9a64869f352199231f00a2
Spencer LaPray <sblapray@yahoo.com>
2019-06-09 21:55:41 -0600

    text input group created for dynamic creation of input types

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import Contact from "./Contact";
-import { Consumer } from "../context";
+import { Consumer } from "../../context";
 
 class Contacts extends Component {
   render() {
