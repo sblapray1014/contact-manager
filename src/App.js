8c8dfffd8d3dbeb50f0e09177b20681825f66810
Spencer LaPray <sblapray@yahoo.com>
2019-06-09 20:20:05 -0600

    bootstrap integrated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
@@ -9,12 +10,18 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="801-855-8555" />
-        <Contact
-          name="Karen Smith"
-          email="karen@gmail.com"
-          phone="801-832-8222"
-        />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="801-855-8555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="801-832-8222"
+          />
+        </div>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = props => {
   const { branding } = props;
   return (
-    <div>
-      <h1>{branding}</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+        <div>
+          <u className="navbar-nav mr-auto">
+            <li className="item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+          </u>
+        </div>
+      </div>
+    </nav>
   );
+};
+
+Header.defaultProps = {
+  branding: "My App"
+};
+
+Header.propTypes = {
+  branding: PropTypes.string.isRequired
 };
 
 export default Header;
